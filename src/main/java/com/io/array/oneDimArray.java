23cdb23dc326e4bc4dd1aec1efadaa3e378ffd23
core <karanamseshubabu28@gmail.com>
2023-07-10 14:43:00 +0530

    corejava

---
+++ src/main/java/com/io/array/oneDimArray.java
@@ -0,0 +1,12 @@
+package com.io.array;
+
+public class oneDimArray {
+	public static void main(String[] args) {
+		int a[] = { 1, 4, 6, 4, 6, 1 };
+		for (int x : a) {
+			System.out.print(" " + x);
+		}
+		System.out.println();
+
+	}
+}
