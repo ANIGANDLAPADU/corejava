8e1e0f63add0c9d3e9c55bd4fb222d4858643630
seshu <karanamseshubabu28@gmail.com>
2023-08-19 19:48:58 +0530

    access specifier

---
+++ src/main/java/accessmodifiers/pp.java
@@ -0,0 +1,10 @@
+package accessmodifiers;
+
+public class pp extends privates {
+	public static void main(String[] args) {
+		pp p = new pp();
+		p.seshu();
+		System.out.println(p.i);
+	}
+
+}
---
+++ src/main/java/accessmodifiers/privates.java
@@ -0,0 +1,10 @@
+package accessmodifiers;
+
+public class privates {
+	protected int i = 90;
+
+	protected void seshu() {
+		System.out.println("Hello");
+	}
+
+}
