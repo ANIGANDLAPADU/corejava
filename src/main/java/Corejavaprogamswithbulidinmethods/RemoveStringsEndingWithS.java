360bcc787149a171cbea7c6ac468b64900fcca62
seshu <karanamseshubabu28@gmail.com>
2023-10-03 20:22:22 +0530

    added

--- src/main/java/Corejavaprogamswithbulidinmethods/RemoveStringsEndingWithS.java
+++ src/main/java/Corejavaprogamswithbulidinmethods/RemoveStringsEndingWithS.java
@@ -1,13 +1,16 @@
 package Corejavaprogamswithbulidinmethods;
 
 public class RemoveStringsEndingWithS {
-    public static void main(String[] args) {
-        String a[] = {"test", "tests", "tester", "testers"};
+	public static void Endswith(String a[]) {
+		for (String str : a) {
+			if (!str.endsWith("s")) {
+				System.out.println(str);
+			}
+		}
+	}
 
-        for (String str : a) {
-            if (!str.startsWith("s")) {
-                System.out.println(str);
-            }
-        }
-    }
+	public static void main(String[] args) {
+		String a[] = { "test", "tests", "tester", "testers"};
+		Endswith(a);
+	}
 }
