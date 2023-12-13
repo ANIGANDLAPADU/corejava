4c632277257ebff5b70b1331445e1774bb8e7af6
seshu <karanamseshubabu28@gmail.com>
2023-12-13 12:28:49 +0530

    added

--- src/main/java/corejavaprogamswithoutbulidmethods/Overridding.java
+++
@@ -1,7 +0,0 @@
-package corejavaprogamswithoutbulidmethods;
-
-public class Overridding {
-	public static void main(String[] args) {
-		    
-	}
-}
---
+++ src/main/java/corejavaprogamswithoutbulidmethods/akhil.java
@@ -0,0 +1,27 @@
+package corejavaprogamswithoutbulidmethods;
+
+ abstract class srinivas {
+	public abstract int seshu();
+
+	public abstract void babu();
+
+}
+
+public class akhil extends srinivas {
+	public static void main(String[] args) {
+
+	}
+
+	@Override
+
+	public int seshu() {
+		return 0;
+
+	}
+
+	@Override
+	public void babu() {
+
+	}
+
+}
