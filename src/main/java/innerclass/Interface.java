ff2799cd6478a6884feeaf14f175efda5be1b111
seshu <karanamseshubabu28@gmail.com>
2024-01-09 12:41:56 +0530

    added?

--- src/main/java/innerclass/Interface.java
+++ src/main/java/innerclass/Interface.java
@@ -1,5 +1,7 @@
 package innerclass;
 
 public interface Interface {
+	int marks = 60;
+
 	public void babu();
 }
--- src/main/java/innerclass/OuterClass.java
+++ src/main/java/innerclass/OuterClass.java
@@ -3,6 +3,10 @@ package innerclass;
 public class OuterClass {
 	private int i = 100;
 
+	public  void outer() {
+		System.out.println("i am from outerclass");
+	}
+
 	public static void main(String[] args) {
 		InnerClass innerClass = new OuterClass().new InnerClass();
 		innerClass.seshu();
@@ -18,17 +22,19 @@ public class OuterClass {
 			System.out.println("hello");
 			System.out.println(i);
 			System.out.println(OuterClass.this.i);
+			OuterClass.this.outer();
 		}
 
 		@Override
 		public void babu() {
-			System.out.println("hello babu");
+
+			System.out.println("hello babu ");
 
 		}
 
 		@Override
 		public void karanam() {
-			System.out.println("hello karanam");
+			System.out.println("hello karanam ");
 
 		}
 
