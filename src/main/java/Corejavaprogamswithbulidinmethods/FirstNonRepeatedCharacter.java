3badd092404b054d886ce9b7cb2929bb9b7d68b2
babu <karanamseshubabu28@gmail.com>
2024-06-27 09:52:08 +0530

    first Non RepeatedCharacter

---
+++ src/main/java/Corejavaprogamswithbulidinmethods/FirstNonRepeatedCharacter.java
@@ -0,0 +1,28 @@
+package Corejavaprogamswithbulidinmethods;
+
+import java.util.HashMap;
+
+public class FirstNonRepeatedCharacter {
+	public static Character firstNonRepeated(String str) {
+
+		HashMap<Character, Integer> hashMap = new HashMap<Character, Integer>();
+		for (char ch : str.toCharArray()) {
+
+			hashMap.put(ch, hashMap.getOrDefault(ch, 0) + 1);
+
+		}
+		for (char ch : str.toCharArray()) {
+			if (hashMap.get(ch) == 1) {
+				return ch;
+			}
+		}
+		return null;
+	}
+
+	public static void main(String[] args) {
+
+		String str = "seshubabukaranam";
+		System.out.println("first Non repeated character is :" + firstNonRepeated(str));
+	}
+
+}
